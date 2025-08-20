413bfa7b6c997897ad1c911586414bbd10c422bb
sudarshan233 <nsridharsudarshan@gmail.com>
2025-08-20 10:33:28 +0530

    Ready

--- frontend/src/Components/Course.jsx
+++ frontend/src/Components/Course.jsx
@@ -5,7 +5,7 @@ const Course = (props) => {
     const { course, progress, description, index, role, enrolled } = props;
     const height = role === 'admin' ? "h-[450px]" : "h-[500px]";
     return (
-        <div className={`hover:w-[24rem] w-80 transition-w flex flex-col ${height} gap-2 rounded bg-white p-4 shadow-lg`}>
+        <div className={`hover:w-[22rem] w-80 transition-w duration-[0.4s] flex-col ${height} gap-2 rounded bg-white p-4 shadow-lg`}>
             <img src='/assets/Learning.gif' alt="Icon for Course" />
             <p className={"text-xl font-bold text-accent"}>{course}</p>
             <p className={"text-sm"}>{description}</p>
--- frontend/src/Components/Stats.jsx
+++ frontend/src/Components/Stats.jsx
@@ -1,8 +1,26 @@
+import metrics from '../../mock/metrics.json';
+import {Doughnut} from "react-chartjs-2";
 
 const Stats  = (props) => {
     const {role} = props;
+    const {admin, student} = metrics;
+    const {orgUsage} = admin;
+    const {activeUsers, totalCourses, completionRate} = orgUsage;
     return (
-        <div></div>
+        <div className={"bg-input w-full h-full flex gap-4 flex-wrap p-10 ml-96"}>
+            <div className={"flex flex-col items-center justify-center w-60 h-40 gap-2 rounded bg-white p-4 shadow-lg"}>
+                <h1 className={"text-xl font-bold"}>Active Users</h1>
+                <p className={"text-accent text-4xl"}>{activeUsers}</p>
+            </div>
+            <div className={"flex flex-col items-center justify-center w-60 h-40 gap-2 rounded bg-white p-4 shadow-lg"}>
+                <h1 className={"text-xl font-bold"}>Completion Rate</h1>
+                <p className={"text-accent text-4xl"}>{completionRate}</p>
+            </div>
+            <div className={"flex flex-col items-center justify-center w-60 h-40 gap-2 rounded bg-white p-4 shadow-lg"}>
+                <h1 className={"text-xl font-bold"}>Total Courses</h1>
+                <p className={"text-accent text-4xl"}>{totalCourses}</p>
+            </div>
+        </div>
     );
 }
 
