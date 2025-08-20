1830daaf6483ee8e0d131b8dddb4ca478d9065ba
sudarshan233 <nsridharsudarshan@gmail.com>
2025-08-20 10:05:13 +0530

    Added Logo

--- frontend/src/Components/Auth.jsx
+++ frontend/src/Components/Auth.jsx
@@ -1,11 +1,13 @@
 import {useState} from "react";
 import {useNavigate} from "react-router";
 
+
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
 
 import users from '../../mock/users.json';
 import toast from 'react-hot-toast';
+import {Blocks} from "lucide-react";
 
 const Auth = (props) => {
     const [auth, setAuth] = useState('login');
@@ -32,7 +34,7 @@ const Auth = (props) => {
     }
     return (
         <div className="flex flex-col justify-center gap-10 w-full h-full box-border p-52">
-            <h1 className="text-5xl self-center my-5">LMS</h1>
+            <h1 className="flex gap-2 items-center text-5xl self-center my-5"><Blocks className={"stroke-accent w-12 h-12"}/> LMS</h1>
             {auth === "login" ? <Login setUserName={setUserName} setPassword={setPassword} checkCredentials={checkCredentials}/> : <Signup/> }
             <p className={"self-center flex gap-2"}>{auth === 'login' ? 'Don\'t have an account?' : 'Already have an account?'}
                 <button className={"text-blue-500 underline"}
