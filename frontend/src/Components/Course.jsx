c3676e3a4347c5605c64c086c45f10f7daf660a5
sudarshan233 <nsridharsudarshan@gmail.com>
2025-08-20 09:50:48 +0530

    Progress: 1. Added Courses for Admin
    Next Steps:
    1. Add Chatbot Q & A for both Admin and Student
    2. Add Stats for both Admin and Student

--- frontend/src/Components/Course.jsx
+++ frontend/src/Components/Course.jsx
@@ -1,19 +1,25 @@
 import StudentProgressBar from "./StudentProgressBar.jsx";
+import {useState} from "react";
 
 const Course = (props) => {
-    const { course, progress, description, index } = props;
+    const { course, progress, description, index, role, enrolled } = props;
+    const height = role === 'admin' ? "h-[450px]" : "h-[500px]";
     return (
-        <div className="flex flex-col h-[500px] w-80 gap-2 rounded bg-white p-4">
+        <div className={`flex flex-col ${height} w-80 gap-2 rounded bg-white p-4 shadow-lg`}>
             <img src='/assets/Learning.gif' alt="Icon for Course" />
             <p className={"text-xl font-bold text-accent"}>{course}</p>
             <p className={"text-sm"}>{description}</p>
-            <div className={"flex gap-2 items-center"}>
-                <p className={"text-xs"}>Course Progress:</p>
-                <div className={'flex w-48'}>
-                    <StudentProgressBar progress={progress}/>
-                </div>
-                <p className={"text-xs"}>{progress}%</p>
-            </div>
+            {role === "student" ?
+                <div className={"flex gap-2 items-center"}>
+                    <p className={"text-xs"}>Course Progress:</p>
+                    <div className={'flex w-48'}>
+                        <StudentProgressBar progress={progress}/>
+                    </div>
+                    <p className={"text-xs"}>{progress}%</p>
+                </div> :  <div className={"flex gap-2 items-center"}>
+                    <p className={"text-xs"}>Students enrolled:</p>
+                    <p className={"text-xs"}>{enrolled.length === undefined ? 0 : enrolled.length} students</p>
+                </div>}
         </div>
     )
 }
--- frontend/src/Components/Courses.jsx
+++ frontend/src/Components/Courses.jsx
@@ -4,12 +4,20 @@ import Course from "./Course.jsx";
 const Courses = (props) => {
     const {admin, student} = metrics;
     const {progress} = student;
+    const {users} = admin;
     const {role} = props;
     return (
         <div className={"bg-input w-full h-full flex gap-4 flex-wrap p-10 ml-96"}>
-            {progress.map((value, index) => {
-                return <Course course={value.course} progress={value.progress} description={value.description} index={index} key={index} />
-            })}
+            {role === "student" ?
+                progress.map((value, index) => {
+                    return <Course course={value.course} progress={value.progress} description={value.description} index={index} key={index}
+                                   role={role}/>
+                }) : users.map((user, index) => {
+                    return <Course course={user.course} enrolled={user.enrolled} description={user.description} index={index} key={index}
+                                   role={role}/>
+                })
+            }
+
         </div>
     )
 }
